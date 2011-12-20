2c03529842085508ec16229f058ac6abf23fda4a
elsdyr <eeeeeeeeeeeeeeeeee@gmail.com>
2011-12-20 12:46:37 +0100

    Utilising random

--- Racer/Servant.c
+++ Racer/Servant.c
@@ -10,10 +10,14 @@ Den kære leder er død.
 
 struct brain {
    u_long random;
+   int i;
    short x,y;
    short foodx[10],foody[10];
    u_long timeout;
-};
+} hukk;
+
+
+
 /*
 struct SquareData {
    u_char NumAnts;
@@ -25,7 +29,12 @@ struct SquareData {
 
 
 int Servant(struct SquareData *felter, struct brain *mem) {
-	return 0 ;
+//	mem->turn++;
+//	return brain.turn % 5;//(brain.random^brain.turn % 4) + 1;
+       //*mem = &hukk;
+int i;
+return (hukk.random)^(hukk.i)++ % 4 ;
+	
 }
 	
 DefineAnt(Servant, "Servant", Servant, struct brain);
